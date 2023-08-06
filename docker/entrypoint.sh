a199eca2f12c5aaba00d21c4f67c2148f0ad1c9e
Garrick Ho <sales@cherryec.com>
2023-08-06 22:28:32 +0800

    .

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-exec soul -d ./data/test.sqlite -p 80
+
+if test $MODE = "TEST"; then
+    echo "Test mode."
+    exec soul -d ./data/test.sqlite -p 80
+else
+    echo "Production mode."
+    exec soul -d ./data/db.sqlite -p 80
+fi
