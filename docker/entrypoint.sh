b3be3648c96a6ce50e7f9d87168c8ef63e0d5bbd
Garrick Ho <sales@cherryec.com>
2023-08-11 17:30:03 +0800

    Updated.

--- docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if test $MODE = "TEST"; then
-    echo "Test mode."
-    exec soul -d ./test.sqlite -p 80
-else
-    echo "Production mode."
-    exec soul -d ./data/db.sqlite -p 80
-fi
